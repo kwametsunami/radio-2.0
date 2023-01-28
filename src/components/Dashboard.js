abb8f1bd97c0287a10a79b27bb5d4ef66ed8d15a
kwametsunami <kwame.kubi@hotmail.com>
2023-01-28 14:25:08 -0500

    added info and login to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 import defaultImage from "../assets/radio.png";
 
 const Dashboard = (props) => {
+  const [showInfo, setShowInfo] = useState(false);
 
-    const favArr = []
+  const infoButton = () => {
+    setShowInfo(!showInfo)
+  }
 
-    favArr.push(props.favouritedStations)
+  const favArr = [];
+
+  favArr.push(props.favouritedStations);
 
   const setDefaultSrc = (event) => {
     event.target.src = defaultImage;
@@ -16,13 +23,32 @@ const Dashboard = (props) => {
       {props.favouritedStations.map((favStation) => {
         return (
           <div className="favItems" key={`${favStation.favourite[0]}`}>
-            <img src={`${favStation.favourite[3]}`} onError={setDefaultSrc}/>
+            <img src={`${favStation.favourite[3]}`} onError={setDefaultSrc} />
             <p>{`${favStation.favourite[1]}`}</p>
             <p>{`${favStation.favourite[5]}`}</p>
             <button value={`${favStation.favourite[2]}`}>play</button>
           </div>
         );
       })}
+      <div className="infoContainer">
+        <div className="infoButtons">
+          <h2>login</h2>
+          <button onClick={infoButton}>
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+        </div>
+        {showInfo ? (
+          <div className="instructions">
+            <button onClick={infoButton}>
+              <i class="fa-solid fa-window-minimize"></i>
+            </button>
+            <p>
+              Click on a marker to get more information on the station. If you
+              like what you see hit play!
+            </p>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
